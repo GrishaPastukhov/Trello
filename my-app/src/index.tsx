eba475100947edf19074694fdc9aecb3cc1526d2
GrishaPastukhov <grishafox505@gmail.com>
2023-06-26 15:44:58 +0300

    routes

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,12 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from './store';
+import Home from './pages/home';
+import Galery from './pages/galery';
+import Register from './pages/register';
+import Login from './pages/login';
+import AddPost from './pages/addPost';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>, 
+  },
+  {
+    path:'/galery',
+    element:<Galery/>,
+  },
+  {
+    path:'/register',
+    element:<Register/>,
+  },
+  {
+    path:'/login',
+    element:<Login/>,
+  },
+  {
+    path:'/addPost',
+    element:<AddPost/>,
+  },
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 );
